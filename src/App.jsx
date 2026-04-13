fbb57d46d71a08f08e93a71c5649146f8ce5cddb
Mouad Boumahdi <mouadboumahdicode@gmail.com>
2026-04-13 11:35:57 +0100

    update weather API key and implement weather fetching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,49 @@ import { useState } from "react";
 function App(){
 
   const [city,setCity] = useState('');
-  const handleSearch = ()=>{
-      console.log("Testing search for: ",city)
-  }
+  const [weather,setWeather] = useState(null)
+  const handleSearch = async()=>{
+   
+ 
   
 
-  const apiKey = import.meta.env.VITE_WEATHER_API_KEY
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+    try{  
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setWeather(data);
+      console.log(data);
+
+
+
+    }catch(error){
+      console.log(error);
+    }
+  }
 
   return(
     <div>
       <h1>Weather app</h1>
       <input type="text" value={city} placeholder="Enter the city name" onChange={(e) => setCity(e.target.value)} />
       <button onClick={handleSearch}>Search</button>
+
+      {weather &&(
+      <div>
+        <h2>{weather.location.name}</h2>
+        <p>Temperature: {weather.current.temp_c}°C</p>
+        <p>Humidity: {weather.current.humidity}°C</p>
+      </div>
+    )};
+
     </div>
+
+    
     
   )
-
+  
 ;
 }
 
