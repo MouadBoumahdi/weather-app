0899c523ffa3151dba4c1089823350dae0cf2a09
Mouad Boumahdi <mouadboumahdicode@gmail.com>
2026-04-13 11:42:35 +0100

    add wind speed display to weather information

--- src/App.jsx
+++ src/App.jsx
@@ -35,8 +35,10 @@ function App(){
       {weather &&(
       <div>
         <h2>{weather.location.name}</h2>
+        <img src={`https:${weather.current.condition.icon}`} alt="" />
         <p>Temperature: {weather.current.temp_c}°C</p>
         <p>Humidity: {weather.current.humidity}°C</p>
+        <p>Wind Speed: {weather.current.wind_kph} km/h</p>
       </div>
     )};
 
